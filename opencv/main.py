40eba6ce756d51e3377fb66c6cd534e565bead4b
sohummehta <sohummehta123@gmail.com>
2025-07-25 15:26:43 -0700

    Testing map coordinates and writing automatically to a json file

--- opencv/main.py
+++ opencv/main.py
@@ -2,14 +2,21 @@ import cv2
 from detect_aruco_marker import detect_aruco
 from homography import compute_global_homography
 import numpy as np
+import json
+import operator
 
-# Define known marker positions on the physical map (in cm or arbitrary units)
+# cm of the map (top left is going to be our origin)
 marker_map = {
     0: [0, 0],       # top-left
     1: [35, 0],     # top-right
     2: [35, 23],    # bottom-right
     3: [0, 23]       # bottom-left
 }
+
+MAP_WIDTH = 35
+MAP_LENGTH = 23
+corner_ids = {0, 1, 2, 3}
+marker_positions = {}
 
 cap = cv2.VideoCapture(0)
 
@@ -27,17 +34,28 @@ while True:
     if H is not None and ids is not None:
         # Draw marker centers in map coordinates (for all markers)
         for marker_corners, marker_id in zip(corners, ids.flatten()):
+            if (marker_id in corner_ids):
+                continue
+
             center = marker_corners[0].mean(axis=0).reshape(1, 1, 2).astype(np.float32)
             map_coord = cv2.perspectiveTransform(center, H)[0][0]
             map_x, map_y = map_coord
 
+            
             # Draw transformed (map) coordinates on screen
-            cv2.putText(frame,
-                        f"Map: ({map_x:.1f}, {map_y:.1f})",
-                        (int(center[0][0][0]), int(center[0][0][1]) + 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
+            if (0 <= map_x <= MAP_WIDTH and 0 <= map_y <= MAP_LENGTH):
+                marker_positions[int(marker_id)] = {
+                    "x" : float(map_x),
+                    "y" : float(map_y)
+                }
+            else :
+                marker_positions.pop(int(marker_id), None)
 
     # Show the annotated video feed
+    with open("marker_positions.json", "w") as f:
+        json.dump(dict(sorted(marker_positions.items())), f, indent=2)
+
+
     cv2.imshow("ArUco Map View", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
