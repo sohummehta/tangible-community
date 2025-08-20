b04fc68f4b59d02dd9fec00b2faf9f5ca700ed3c
sohummehta <sohummehta123@gmail.com>
2025-08-20 10:23:52 -0700

    Updating main page.tsx

--- frontend/src/app/main/page.tsx
+++ frontend/src/app/main/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Mapping from "@/components/mapping"
-import {Typography } from "@mui/material"
+import {Typography, Button, Box } from "@mui/material"
 import Grid from "@mui/material/Grid"
 import Understand from '@/components/understand';
 
@@ -16,7 +16,11 @@ export default function Main() {
             <Grid size ={{xs: 12, md: 8}}>
                 <Grid container direction = "column" spacing = {0.5} sx = {{height: "100vh"}}>
                     <Grid sx = {{border: "1px solid black", height: "62.5vh"}}>
+                        <Typography align = "center" variant = "h6">Mapping</Typography>
                         <Mapping />
+                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: "10px" }}>
+                            <Button variant = "contained" color= "primary">Change View</Button>
+                        </Box>
                     </Grid>
 
                     <Grid sx = {{border : "1px solid black", height: "37vh"}}>
