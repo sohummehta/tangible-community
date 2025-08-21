ed19350b1b2e0b6ff0ed87f59ca473891d591ef5
sohummehta <sohummehta123@gmail.com>
2025-08-21 14:09:44 -0700

    Fixing backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -67,6 +67,10 @@ DATABASES = {
     'default': {
         'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
         'NAME': config('DB_NAME', default=os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default=''),
+        'PORT': config('DB_PORT', default=''),
     }
 }
 
